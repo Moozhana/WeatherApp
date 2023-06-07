aa1bdd7f22af7f6625ed00f9f0d3dda487d4cef3
moozhana <moozhan.pnv@gmail.com>
2023-06-07 10:20:42 +0330

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,64 @@
+let currentTime = new Date();
+
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentTime.getDay()];
+let header4 = document.querySelector("h4");
+header4.innerHTML = `${day} ${hours}:${minutes}`;
+
+function showTemp(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let header2 = document.querySelector(".h2");
+  header2.innerHTML = temperature;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#speed");
+  wind.innerHTML = Math.round(response.data.wind.speed);
+  console.log(response);
+}
+
+function showCity(event) {
+  event.preventDefault();
+  let citySearch = document.querySelector(".text");
+  let header1 = document.querySelector("h1");
+  header1.innerHTML = `${citySearch.value}`;
+  let units = "metric";
+  let apiKey = "2513f3c728b1b5ff4f4347e1a6af22b8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${citySearch.value}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(showTemp);
+}
+let search = document.querySelector(".form");
+search.addEventListener("submit", showCity);
+
+function positioning(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey2 = "2513f3c728b1b5ff4f4347e1a6af22b8";
+  let apiUrl2 = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey2}&units=metric`;
+  axios.get(apiUrl2).then(display);
+}
+navigator.geolocation.getCurrentPosition(positioning);
+
+function display(response) {
+  let headerName = document.querySelector("h1");
+  headerName.innerHTML = response.data.name;
+  let temper = Math.round(response.data.main.temp);
+  let headerTemp = document.querySelector(".h2");
+  headerTemp.innerHTML = temper;
+  let humidityCurrent = document.querySelector("#humidity");
+  humidityCurrent.innerHTML = response.data.main.humidity;
+  let windCurrent = document.querySelector("#speed");
+  windCurrent.innerHTML = Math.round(response.data.wind.speed);
+}
+
+let current = document.querySelector(".current");
+current.addEventListener("submit", display);
