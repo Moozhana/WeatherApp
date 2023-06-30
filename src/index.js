68d8783519c5bbaf90218d32f33bef451037ac68
moozhana <moozhan.pnv@gmail.com>
2023-06-30 06:24:07 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -68,7 +68,7 @@ function displayForecast(response) {
 }
 
 function getForecast(coords) {
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiKey = "63214c4281922e3bb72fdf12dada7734";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -98,7 +98,7 @@ function showWeather(response) {
 
 function searchCity(city) {
   let units = "metric";
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiKey = "63214c4281922e3bb72fdf12dada7734";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   axios.get(apiUrl).then(showWeather);
 }
@@ -114,7 +114,7 @@ search.addEventListener("submit", handleSubmit);
 function searchLocation(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
-  let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
+  let apiKey = "63214c4281922e3bb72fdf12dada7734";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
 }
